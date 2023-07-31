b76b7b942739841c50b02e1fe1540a039481806a
Minnnning <6alswjd6@gmail.com>
2023-07-31 10:37:44 +0900

    first init

---
+++ src/test/java/com/mygroup/sbb/SbbApplicationTests.java
@@ -0,0 +1,29 @@
+package com.mygroup.sbb;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mygroup.sbb.question.Question;
+import com.mygroup.sbb.question.QuestionRepository;
+
+
+@SpringBootTest
+class SbbApplicationTests {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void testJpa() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        q.setSubject("수정된 제목");
+        this.questionRepository.save(q);
+    }
+}
