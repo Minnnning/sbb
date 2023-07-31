91f5641d3aa2590797f4e24605b4a4d67c46d88b
Minnnning <6alswjd6@gmail.com>
2023-07-31 15:44:32 +0900

    entity add and login need method modify

--- src/main/java/com/mygroup/sbb/answer/AnswerService.java
+++ src/main/java/com/mygroup/sbb/answer/AnswerService.java
@@ -1,10 +1,13 @@
 package com.mygroup.sbb.answer;
 
-import com.mygroup.sbb.question.Question;
-import lombok.RequiredArgsConstructor;
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import com.mygroup.sbb.question.Question;
+import com.mygroup.sbb.user.SiteUser;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
@@ -13,11 +16,12 @@ public class AnswerService {
     private final AnswerRepository answerRepository;
 
 
-    public void create(Question question, String content) {
+    public void create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
+        answer.setAuthor(author);
         this.answerRepository.save(answer);
     }
 }
--- src/main/java/com/mygroup/sbb/question/Question.java
+++ src/main/java/com/mygroup/sbb/question/Question.java
@@ -4,15 +4,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.mygroup.sbb.answer.Answer;
+import com.mygroup.sbb.user.SiteUser;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -34,4 +35,7 @@ public class Question {
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
+
+    @ManyToOne
+    private SiteUser author;
 }
