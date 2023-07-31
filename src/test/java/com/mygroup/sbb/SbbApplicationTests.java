3f8eb82f7c94fac120f6163799b81c65b6e18962
Minnnning <6alswjd6@gmail.com>
2023-07-31 11:55:14 +0900

    add paging

--- src/test/java/com/mygroup/sbb/SbbApplicationTests.java
+++ src/test/java/com/mygroup/sbb/SbbApplicationTests.java
@@ -1,29 +1,23 @@
 package com.mygroup.sbb;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.mygroup.sbb.question.Question;
-import com.mygroup.sbb.question.QuestionRepository;
-
+import com.mygroup.sbb.question.QuestionService;
 
 @SpringBootTest
 class SbbApplicationTests {
 
     @Autowired
-    private QuestionRepository questionRepository;
+    private QuestionService questionService;
 
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(1);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-        q.setSubject("수정된 제목");
-        this.questionRepository.save(q);
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
     }
 }
