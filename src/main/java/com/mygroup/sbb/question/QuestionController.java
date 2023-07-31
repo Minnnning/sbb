5b96db5c8b53840376eb754b1a8846ca2472c562
Minnnning <6alswjd6@gmail.com>
2023-07-31 16:20:42 +0900

    add question delete

--- src/main/java/com/mygroup/sbb/question/QuestionController.java
+++ src/main/java/com/mygroup/sbb/question/QuestionController.java
@@ -1,25 +1,26 @@
 package com.mygroup.sbb.question;
 
-import java.security.Principal;
-
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.mygroup.sbb.answer.AnswerForm;
+
+import java.security.Principal;
 import com.mygroup.sbb.user.SiteUser;
 import com.mygroup.sbb.user.UserService;
 
 import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
+
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/question")
@@ -86,5 +87,16 @@ public class QuestionController {
         }
         this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.questionService.delete(question);
+        return "redirect:/";
     }
 }
